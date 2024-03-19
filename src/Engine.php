1c168874af21d3469c6e85700e6d758880c47859
Ahilesov_loc <contextus.lab@ya.ru>
2024-03-19 19:49:59 +0500

    Commit updated Cli.php and Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -8,7 +8,7 @@ use function cli\prompt;
 const NUMBER_OF_ROUNDS = 3;
 function startGame($taskText, $getDataGame): void
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($taskText);
