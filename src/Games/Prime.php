38869ef67a08935e2b6ca9e0692974913464b268
Ahilesov_loc <contextus.lab@ya.ru>
2024-03-20 14:13:09 +0500

    Commit updated brain-prime

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -6,8 +6,12 @@ use function BrainGames\Engine\startGame;
 
 const TASK_TEXT = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-function isPrime($num)
+function isPrime($num): bool
 {
+    if ($num == 1) {
+        return false;
+    }
+
     for ($i = 2; $i <= sqrt($num); $i++) {
         if (!($num % $i)) {
             return false;
@@ -20,7 +24,7 @@ function basicLogicGame(): void
 {
     $getDataGame = function () {
 
-        $question = rand(1, 20);
+        $question = rand(1, 3);
         $answer = (isPrime($question) ? 'yes' : 'no');
 
         return [$question, $answer];
